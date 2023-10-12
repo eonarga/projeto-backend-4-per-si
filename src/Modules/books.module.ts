98bda95c685f0eba2828460450b99da946d0af7f
Assis Policarpo <assisneto013@gmail.com>
2023-10-11 21:43:23 -0400

    module import

--- src/Modules/books.module.ts
+++ src/Modules/books.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BooksController } from 'src/Controllers/books.controller';
 import { BooksService } from 'src/Services/books.service';
 import { BookSchema } from 'src/Mongo/Schemas/book.schema';
+import { BookRepository } from '../Mongo/Repository/book.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Book', schema: BookSchema }])],
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, BookRepository],
 })
 export class BooksModule {}
